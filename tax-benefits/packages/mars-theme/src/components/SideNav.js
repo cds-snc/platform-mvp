1cca40b7270700578a787c4bd68795882958947c
Tim Arney <tim@line37.com>
2021-07-06 14:10:26 -0400

    fix active nav

--- tax-benefits/packages/mars-theme/src/components/SideNav.js
+++ tax-benefits/packages/mars-theme/src/components/SideNav.js
@@ -1,6 +1,17 @@
 import React from "react";
-import { connect} from "frontity";
+import { connect } from "frontity";
 import Link from "@frontity/components/link";
+
+const isActive = (state, link) => {
+  const current = state.router.link;
+
+  console.log("current:", `${state.source.url}${current}`,"link:",link)
+  
+  if (`${state.source.url}${current}` === link) {
+    return "sidebar-nav-active";
+  }
+  return null;
+};
 
 const SideNav = ({ state }) => {
   const items = state.source.get(`/menu/${state.theme.menuUrl}/`).items;
@@ -14,35 +25,33 @@ const SideNav = ({ state }) => {
       <p className="intro">{description}</p>
       <nav className="sidebar-nav">
         <ul>
-        {items.map((item) => {
-          if (!item.child_items) {
-            return (
-              <li key={item.ID}>
-                <Link  link={item.url}>
-                  {item.title}
-                </Link>
-              </li>
-            );
-          } else {
-            const childItems = item.child_items;
-            return (
-              <li key={item.ID}>
-                 <Link link={item.url}>{item.title}</Link>
-                <ul className="sub-menu">
-                  {childItems.map((childItem) => {
-                    return (
-                      <li className="sub-link" key={childItem.ID}>
-                        <Link  link={childItem.url}>
-                          {childItem.title}
-                        </Link>
-                      </li>
-                    );
-                  })}
-                </ul>
-              </li>
-            );
-          }
-        })}
+          {items.map((item) => {
+            if (!item.child_items) {
+              return (
+                <li key={item.ID} className={isActive(state, item.url)}>
+                  <Link link={item.url} >
+                    {item.title}
+                  </Link>
+                </li>
+              );
+            } else {
+              const childItems = item.child_items;
+              return (
+                <li key={item.ID} className={isActive(state, item.url)}>
+                  <Link link={item.url}>{item.title}</Link>
+                  <ul className="sub-menu">
+                    {childItems.map((childItem) => {
+                      return (
+                        <li className={`sub-link ${isActive(state, childItem.url)}`} key={childItem.ID}>
+                          <Link link={childItem.url}>{childItem.title}</Link>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </li>
+              );
+            }
+          })}
         </ul>
       </nav>
     </aside>
