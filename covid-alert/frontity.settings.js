3f80e3214d83dd2851d17c25b4e8e2746fe3b823
Tim Arney <tim@line37.com>
2021-07-08 12:50:08 -0400

    test match

--- covid-alert/frontity.settings.js
+++ covid-alert/frontity.settings.js
@@ -1,39 +1,57 @@
-const settings = {
-  name: "covid-alert",
-  state: {
-    frontity: {
-      title: "-",
-      description: "-",
+const settings = [
+  {
+    name: "covid-alert",
+    match: ["(?!/fr)"],
+    state: {
+      frontity: {
+        title: "-",
+        description: "-",
+      },
     },
-  },
-  packages: [
-    {
-      name: "@cdssnc/cds-docs-theme",
-      state: {
-        theme: {
-          menu: [],
-          featured: {
-            showOnList: false,
-            showOnPost: false,
+    packages: [
+      {
+        name: "@frontity/wp-source",
+        state: {
+          source: {
+            url: "https://platform.digital.canada.ca/covid-alert/",
+            homepage: "/home",
           },
         },
       },
+      {
+        name: "@cdssnc/cds-docs-theme",
+      },
+      "@frontity/tiny-router",
+      "@frontity/html2react",
+    ],
+  },
+  {
+    name: "covid-alert-fr",
+    match: ["/fr/"],
+    state: {
+      frontity: {
+        title: "-",
+        description: "-",
+      },
     },
-    {
-      name: "@frontity/wp-source",
-      state: {
-        source: {
-          url: "https://platform.digital.canada.ca/covid-alert/",
-          homepage:"/home",
+    packages: [
+      {
+        name: "@frontity/wp-source",
+        state: {
+          source: {
+            url: "https://platform.digital.canada.ca/covid-alert/fr",
+            homepage: "/home",
+          },
         },
       },
-      
-    },
-    "@frontity/tiny-router",
-    "@frontity/html2react",
-  ],
-};
+      {
+        name: "@cdssnc/cds-docs-theme",
+      },
+      "@frontity/tiny-router",
+      "@frontity/html2react",
+    ],
+  },
+];
 
 export default settings;
-
-//https://platform.digital.canada.ca/covid-alert/fr/
+// ?frontity_name=covid-alert-fr
