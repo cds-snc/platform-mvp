bdbc9517e28d1dba8473c994b7ad4bc4df7a62cc
Tim Arney <tim@line37.com>
2021-07-09 15:36:14 -0400

    update server

--- server/handler.js
+++ server/handler.js
@@ -1,23 +1,32 @@
-const serverless = require("serverless-http");
-const express = require("express");
-const app = express();
+const awsServerlessExpress = require("aws-serverless-express");
+const app = require("./build/server.js").default;
+const server = awsServerlessExpress.createServer(app);
+const path = require("path");
+const FileHandler = require("serverless-aws-static-file-handler");
 
-app.get("/", (req, res, next) => {
-  return res.status(200).json({
-    message: "Hello from root!",
-  });
-});
+// Handle routing to Frontity Server
+exports.handler = (event, context) => {
+  awsServerlessExpress.proxy(server, event, context);
+};
 
-app.get("/hello", (req, res, next) => {
-  return res.status(200).json({
-    message: "Hello from path!",
-  });
-});
+// Handle returning all static files located in `/build/static/`
+const StaticFilesPath = path.join(__dirname, "./build/static/");
+const staticFileHandler = new FileHandler(StaticFilesPath);
+exports.static = async (event, context) => {
+  if (!event.path.startsWith("/static/")) {
+    throw new Error(`[404] Invalid filepath for this resource: ${fname}`);
+  }
+  let response = await staticFileHandler.get(event, context);
+  response.headers = {
+    'Content-Type': response.headers["Content-Type"],
+    'Cache-Control': 'max-age=31536000',
+ }
+ return response;
+};
 
-app.use((req, res, next) => {
-  return res.status(404).json({
-    error: "Not Found",
-  });
-});
-
-module.exports.handler = serverless(app);
+// Handle returning `favicon.ico` file in the project root
+const RootFilesPath = path.join(__dirname);
+const rootFileHandler = new FileHandler(RootFilesPath);
+exports.favicon = async (event, context) => {
+  return rootFileHandler.get(event, context);
+}
