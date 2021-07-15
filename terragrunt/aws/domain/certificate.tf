1077cd5e0537b8b2c1dbc7e6b9f40e8dd8e8473c
Pat Heard <patrick.heard@cds-snc.ca>
2021-07-14 20:00:53 -0400

    feat: switch to platform.cdssandbox.xyz domain
    There are additional AWS validation steps we need for the
    docs.digital.canada.ca cert because it's getting caught up in
    "Alexa 1,000 most popular sites" fraud detection logic.
    
    In the interest of speed, we're switching to a cdssandbox.xyz
    subdomain to test.

--- terragrunt/aws/domain/certificate.tf
+++ terragrunt/aws/domain/certificate.tf
@@ -1,4 +1,4 @@
-resource "aws_acm_certificate" "docs_platform_mvp" {
+resource "aws_acm_certificate" "platform_mvp" {
   domain_name               = var.domain_name
   subject_alternative_names = ["*.${var.domain_name}"]
   validation_method         = "DNS"
@@ -6,13 +6,27 @@ resource "aws_acm_certificate" "docs_platform_mvp" {
   tags = {
     (var.billing_tag_key) = var.billing_tag_value
   }
-
-  lifecycle {
-    create_before_destroy = true
-  }
 }
 
-resource "aws_acm_certificate_validation" "docs_platform_mvp" {
-  certificate_arn         = aws_acm_certificate.docs_platform_mvp.arn
-  validation_record_fqdns = [for record in aws_route53_record.docs_platform_mvp_validation : record.fqdn]
+resource "aws_route53_record" "platform_mvp_validation" {
+  zone_id = aws_route53_zone.platform_mvp.zone_id
+
+  for_each = {
+    for dvo in aws_acm_certificate.platform_mvp.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  type            = each.value.type
+  ttl             = 60
+}
+
+resource "aws_acm_certificate_validation" "platform_mvp" {
+  certificate_arn         = aws_acm_certificate.platform_mvp.arn
+  validation_record_fqdns = [for record in aws_route53_record.platform_mvp_validation : record.fqdn]
 }
--- terragrunt/aws/domain/data.tf
+++ terragrunt/aws/domain/data.tf
@@ -1,3 +1,3 @@
-data "aws_api_gateway_rest_api" "docs_platform_mvp" {
+data "aws_api_gateway_rest_api" "platform_mvp" {
   name = var.api_name
 }
