7cb9ed91a776787ac646aa0e8707ec7552ce7dcd
Tim Arney <tim@line37.com>
2021-07-15 11:13:18 -0400

    break each sites settings out to individual files

---
+++ cds-snc-docs/config/covid-alert.js
@@ -0,0 +1,31 @@
+export const covidAlert = {
+  name: "covid-alert",
+  match: [
+    ".*https?://localhost:3000",
+    ".*https?://platform-covid-alert.herokuapp.com",
+  ],
+  state: {
+    frontity: {
+      title: "Covid Alert",
+      description:
+        "COVID Alert is Canada's free COVID-19 exposure notification app.",
+      url: "https://localhost:3000",
+    },
+  },
+  packages: [
+    {
+      name: "@frontity/wp-source",
+      state: {
+        source: {
+          url: "https://platform.digital.canada.ca/covid-alert",
+          homepage: "/home",
+        },
+      },
+    },
+    {
+      name: "cds-docs-theme",
+    },
+    "@frontity/tiny-router",
+    "@frontity/html2react",
+  ],
+};
--- cds-snc-docs/frontity.settings.js
+++ cds-snc-docs/frontity.settings.js
@@ -1,65 +1,7 @@
-const settings = [
-  {
-    name: "covid-alert",
-    match: [
-      ".*https?://localhost:3000",
-      ".*https?://platform-covid-alert.herokuapp.com",
-    ],
-    state: {
-      frontity: {
-        title: "Covid Alert",
-        description: "COVID Alert is Canada's free COVID-19 exposure notification app.",
-        url: "https://localhost:3000",
-      },
-    },
-    packages: [
-      {
-        name: "@frontity/wp-source",
-        state: {
-          source: {
-            url: "https://platform.digital.canada.ca/covid-alert",
-            homepage: "/home",
-          },
-        },
-      },
-      {
-        name: "cds-docs-theme",
-      },
-      "@frontity/tiny-router",
-      "@frontity/html2react",
-    ],
-  },
-  {
-    name: "covid-alert-fr",
-    match: [
-      ".*https?://localhost:4000",
-      ".*https?://platform-covid-alert-fr.herokuapp.com",
-    ],
-    state: {
-      frontity: {
-        title: "Alerte COVID",
-        description:
-          "Alerte COVID est l'application gratuite de notification d'exposition du Canada.",
-      },
-    },
-    packages: [
-      {
-        name: "@frontity/wp-source",
-        state: {
-          source: {
-            url: "https://platform.digital.canada.ca/covid-alert/fr",
-            homepage: "/home-fr",
-          },
-        },
-      },
-      {
-        name: "cds-docs-theme",
-      },
-      "@frontity/tiny-router",
-      "@frontity/html2react",
-    ],
-  },
-];
+import { covidAlert } from "./config/covid-alert";
+import { covidAlertFr } from "./config/covid-alert-fr";
+
+const settings = [covidAlert, covidAlertFr];
 
 export default settings;
 // ?frontity_name=covid-alert-fr
