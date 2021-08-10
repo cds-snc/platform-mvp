1bffca320af8885646b33eae9ee88138a1873597
Tim Arney <tim@line37.com>
2021-08-10 12:57:16 -0400

    add block example

--- blocks/cds-expander/index.php
+++ blocks/cds-expander/index.php
@@ -1,13 +1,13 @@
 <?php
 
 /**
- * Plugin Name: Gutenberg Examples Basic EsNext
- * Plugin URI: https://github.com/WordPress/gutenberg-examples
- * Description: This is a plugin demonstrating how to register new blocks for the Gutenberg editor.
+ * Plugin Name: CDS-SNC Expander
+ * Plugin URI: https://github.com/cds-snc/platform-mvp
+ * Description: Expander plugin
  * Version: 1.1.0
- * Author: the Gutenberg Team
+ * Author: Tim Arney
  *
- * @package gutenberg-examples
+ * @package cds-snc-blocks
  */
 
 defined( 'ABSPATH' ) || exit;
@@ -15,10 +15,10 @@ defined( 'ABSPATH' ) || exit;
 /**
  * Load all translations for our plugin from the MO file.
 */
-add_action( 'init', 'gutenberg_examples_01_esnext_load_textdomain' );
+add_action( 'init', 'cds_textdomain' );
 
-function gutenberg_examples_01_esnext_load_textdomain() {
-	load_plugin_textdomain( 'gutenberg-examples', false, basename( __DIR__ ) . '/languages' );
+function cds_textdomain() {
+	load_plugin_textdomain( 'cds-snc-expander', false, basename( __DIR__ ) . '/languages' );
 }
 
 /**
@@ -27,20 +27,20 @@ function gutenberg_examples_01_esnext_load_textdomain() {
  *
  * Passes translations to JavaScript.
  */
-function gutenberg_examples_01_esnext_register_block() {
+function cds_register_block() {
 
 	// automatically load dependencies and version
 	$asset_file = include( plugin_dir_path( __FILE__ ) . 'build/index.asset.php');
 
 	wp_register_script(
-		'gutenberg-examples-01-esnext',
+		'cds-snc-expander',
 		plugins_url( 'build/index.js', __FILE__ ),
 		$asset_file['dependencies'],
 		$asset_file['version']
 	);
 
-	register_block_type( 'gutenberg-examples/example-01-basic-esnext', array(
-		'editor_script' => 'gutenberg-examples-01-esnext',
+	register_block_type( 'cds-snc/expander', array(
+		'editor_script' => 'cds-snc-expander',
 	) );
 
   if ( function_exists( 'wp_set_script_translations' ) ) {
@@ -49,8 +49,8 @@ function gutenberg_examples_01_esnext_register_block() {
      * plugin_dir_path( MY_PLUGIN ) . 'languages' ) ). For details see
      * https://make.wordpress.org/core/2018/11/09/new-javascript-i18n-support-in-wordpress/
      */
-    wp_set_script_translations( 'gutenberg-examples-01-esnext', 'gutenberg-examples' );
+    wp_set_script_translations( 'cds-snc-expander', 'expander' );
   }
 
 }
-add_action( 'init', 'gutenberg_examples_01_esnext_register_block' );
+add_action( 'init', 'cds_register_block' );
