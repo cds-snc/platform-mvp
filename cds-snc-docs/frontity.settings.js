3fa222d5e3bed1c14fcfb6657d4fd356b731f880
Tim Arney <timarney@users.noreply.github.com>
2021-08-11 15:35:02 -0400

    add learning site (#38)

--- cds-snc-docs/frontity.settings.js
+++ cds-snc-docs/frontity.settings.js
@@ -1,8 +1,9 @@
 import { main } from "./config/main";
 import { covidAlert } from "./config/covid-alert";
 import { alerteCovid } from "./config/alerte-covid";
+import { cdsLearning } from "./config/cds-learning";
 
-const settings = [main, covidAlert, alerteCovid];
+const settings = [main, covidAlert, alerteCovid, cdsLearning];
 
 export default settings;
 // ?frontity_name=covid-alert-fr
