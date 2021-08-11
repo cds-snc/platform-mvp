54c546e165ab2043fb7e3743631d1b554edb4a4a
Tim Arney <tim@line37.com>
2021-08-11 15:34:30 -0400

    add learning site

---
+++ cds-snc-docs/config/cds-learning.js
@@ -0,0 +1,31 @@
+export const cdsLearning = {
+  name: "cds-learning",
+  match: [".*https?://localhost:10000", "\/cds-learning"],
+  state: {
+    frontity: {
+      title: "CDS Learning",
+      description:
+        "-",
+      url: "https://platform.digital.canada.ca/cds-learning/",
+      menuUrl: "main-fr",
+      footerUrl: "footer-fr",
+      languageUrl: "language-fr",
+    },
+  },
+  packages: [
+    {
+      name: "@frontity/wp-source",
+      state: {
+        source: {
+          url: "https://platform.digital.canada.ca/cds-learning",
+          homepage: "/cds-learning",
+        },
+      },
+    },
+    {
+      name: "cds-docs-theme",
+    },
+    "@frontity/tiny-router",
+    "@frontity/html2react",
+  ],
+};
