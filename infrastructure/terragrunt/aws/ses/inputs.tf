025266af435966224c7f06c8958d90e6d8f3700d
Pat Heard <patrick.heard@cds-snc.ca>
2021-08-12 18:40:09 +0000

    feat: add SPF and DMARC validation DNS records
    This creates the SPF validation records and DMARC authentication
    record used to improve the behaviour of sending email through SES.

--- infrastructure/terragrunt/aws/ses/inputs.tf
+++ infrastructure/terragrunt/aws/ses/inputs.tf
@@ -1,9 +1,24 @@
+variable "dmarc_policy" {
+  description = "DMARC policy action to take for emails that fail authentication"
+  type        = string
+
+  validation {
+    condition     = contains(["none", "quarantine", "reject"], var.dmarc_policy)
+    error_message = "Valid values for var: dmarc_policy are: none, quarantine, reject."
+  }
+}
+
+variable "dmarc_report_email" {
+  description = "DMARC destination email address for reports of emails that failed authentication"
+  type        = string
+}
+
 variable "route53_zone_id" {
   description = "Route53 zone to create the SES verification and DKIM records in"
   type        = string
 }
 
 variable "ses_sending_domain" {
-  description = "Domain that will be used by SES to send emails from."
+  description = "Domain that will be used by SES to send emails from"
   type        = string
 }
