87b9c5586924b8c24b71729962188a2a3cbfa105
Tim Arney <tim@line37.com>
2021-08-13 12:38:11 -0400

    wip

--- front-end/packages/wett-theme/processors/gridColumns.js
+++ front-end/packages/wett-theme/processors/gridColumns.js
@@ -9,6 +9,12 @@ export const gridColumns = {
   processor: ({ node }) => {
     if (node.props.className.indexOf("wp-block-columns") !== -1) {
       console.log("children.length", node.children.length);
+      
+      if(node.children.length){
+        
+      }
+
+      //node.children.props.className = "provisional gc-table table wb-tables";
     }
     return node;
   },
