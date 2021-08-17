52cd9959c6f5b2709f248a78b329c58fcdc022cf
Tim Arney <tim@line37.com>
2021-08-17 10:56:25 -0400

    cypress setup

--- wordpress/wp-content/plugins/cds-base/cypress/support/index.js
+++ wordpress/wp-content/plugins/cds-base/cypress/support/index.js
@@ -18,3 +18,7 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+import { registerWpTestCommands } from 'cypress-wp-test-utils';
+
+registerWpTestCommands();
