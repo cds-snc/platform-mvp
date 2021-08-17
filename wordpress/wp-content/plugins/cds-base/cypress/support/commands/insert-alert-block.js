b34b2e1f265be5fbdd7d80e4aa76aa3e8836ba00
Tim Arney <tim@line37.com>
2021-08-17 11:02:50 -0400

    fix test

--- wordpress/wp-content/plugins/cds-base/cypress/support/commands/insert-alert-block.js
+++ wordpress/wp-content/plugins/cds-base/cypress/support/commands/insert-alert-block.js
@@ -1,6 +1,6 @@
 Cypress.Commands.add('insertAlertBlock', () => {
     cy.searchForBlock('Alert');
     cy.get(
-        'button.editor-block-list-item-cds-snc-blocks-button'
+        'button.editor-block-list-item-cds-snc-alert'
     ).click({ force: true });
 });
