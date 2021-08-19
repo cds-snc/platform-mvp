2bf9488156a92a542e23582b647acec26b02502e
Tim Arney <tim@line37.com>
2021-08-19 10:48:49 -0400

    add date filter and package to parse html dom

--- wordpress/wp-content/themes/cds-default/functions.php
+++ wordpress/wp-content/themes/cds-default/functions.php
@@ -2,6 +2,9 @@
 
 declare(strict_types=1);
 
+require __DIR__ . '/vendor/autoload.php';
+require_once(__DIR__."/inc/template-filters.php");
+
 /**
  * cds-default functions and definitions
  *
---
+++ wordpress/wp-content/themes/cds-default/inc/template-filters.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+use PHPHtmlParser\Dom;
+
+add_filter( 'render_block', 'cds_date_block', 10, 3);
+ 
+function cds_date_block( $block_content, $block ) {
+     
+  if( "core/post-date" !== $block['blockName'] ) {
+    return $block_content;
+  }
+
+  try{
+      $dom = new Dom;
+      $dom->loadStr($block_content);
+      $time = $dom->find('time')[0];
+      return str_replace($time, "[".$time."]", $block_content);
+  }catch(Exception $e){
+      return $block_content;
+  }
+
+}
