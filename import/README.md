42d8f5f64b8c033f8ed1fb78573a510d3407f3ee
Daine Trinidad <daine.trinidad@cds-snc.ca>
2021-08-24 23:48:06 -0700

    Major changes to running the script, and parse body for a better title

--- import/README.md
+++ import/README.md
@@ -13,3 +13,8 @@ example:
 ```bash
 npm run import --file=imports/export_public_templates.json
 ```
+
+For debugging, this was an easy command for me to use:
+```bash
+(npm run import --file=imports/export_public_templates.json | tee import.log) 3>&1 1>&2 2>&3 | tee import.error.log
+```
--- import/importer.mjs
+++ import/importer.mjs
@@ -3,30 +3,56 @@ import { spawn } from 'child_process';
 import markdownConverter from "./markdown-converter.mjs";
 
 export default class Importer {
+
+    static importCount = {
+        success: 0,
+        failure: 0,
+        skipped: 0,
+        total: 0
+    }
+
     static processNotifyTemplate = (filename) => {
         let rawdata = readFileSync(filename);
         let contents = JSON.parse(rawdata);
 
-        const getTitle = (title) => {
-            // Title sometimes starts with '# ', so we clean it up
-            if(title.startsWith("# ")){
-                return title.substring(2)
+        const getTitleAndBody = (subject, content) => {
+            let title = content.split('\r\n')[0]
+            
+            if(title.length >= 100 && subject){
+                title = subject
+            }else{
+                content = content.substring(title.length).trim()
             }
-            return title
+
+            if(title.startsWith("#")){
+                title = title.substring(1).trim()
+            }
+
+            return {
+                title: title,
+                body: markdownConverter(content)
+            }
         }
+
+        Importer.importCount.total = contents.length
     
         contents.forEach(item => {
             const post = {
-                title: getTitle(item.subject),
-                body: markdownConverter(item.content),
+                ...getTitleAndBody(item.subject, item.content),
                 name: item.name,
                 createDate: item.created_at
-            }    
-            Importer.wpPostCreate(post)
+            }
+            if( item.archived ){
+                Importer.importCount.skipped++
+            } else {
+                Importer.wpPostCreate(post)
+            }
         })
+
     }
 
-    static wpPostCreate = ({body, title, name, createDate}) => {
+    static wpPostCreate = ({body, title, name, createDate}, attempts = 0) => {
+    
 
         const getFormattedDate = (dateStr) => {
             // returns formatted date string yyyy-mm-dd hh:mm:ss
@@ -40,6 +66,13 @@ export default class Importer {
             return ''
         }
 
+        // Pause spawning for 3 secs for every 30 commands
+        if(Importer.importCount % 30 == 0){
+            setTimeout(() => {
+                console.log('pausing...')
+            }, 3000)
+        }
+
         const create_post = spawn("docker", ["exec", "cli", "/usr/local/bin/wp", "--path=/var/www/html", 
             "post", "create",
             `--post_content=${body}`,
@@ -50,12 +83,38 @@ export default class Importer {
 
         // WP CLI output
         create_post.stdout.on('data', (data) => {
-            console.log(data.toString());
+            if(data.toString().includes("Success: Created post")){
+                Importer.importCount.success++
+            }
+            console.log({
+                'title': title,
+                // 'item': body,
+                'msg': data.toString(),
+                'count': `${Importer.importCount.success} (${Importer.importCount.skipped}) / ${Importer.importCount.total}`
+            })
         });
 
         // WP CLI Errors
         create_post.stderr.on('data', (data) => {
-            console.error("ERROR", data.toString());
+            const maxAttempts = 3
+            if( attempts < maxAttempts ){
+                // If we spawned too many commands, the database goes away, but it restarts, so we attempt to retry
+                console.error('retrying...', attempts)
+                setTimeout(() => {
+                    console.error('retrying', attempts+1)
+                    Importer.wpPostCreate({body, title, name, createDate}, attempts + 1)
+                }, 3000)
+                
+            } else {
+                // Fail after maxAttempts
+                Importer.importCount.failure++
+                console.error({
+                    'title': title,
+                    // 'item': body,
+                    'msg': `ERROR: ${data.toString()}`,
+                    'count': `${Importer.importCount.failure} / ${Importer.importCount.total}`
+                })
+            }
         });
     }
 }
