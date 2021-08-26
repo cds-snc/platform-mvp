7ca784c65cbaafa8a5f42077f40293aaab6798a5
Tim Arney <timarney@users.noreply.github.com>
2021-08-26 15:44:12 -0400

    Clean up WP admin + Login Screen (#98)
    login
    meta-boxes
    menus
    notices
    admin bar + tabs
    rename Posts to Articles
    profile page
    dashboard (added a welcome widget)
    added admin -> css + js files
    added composer.json

--- wordpress/wp-content/plugins/cds-base/index.php
+++ wordpress/wp-content/plugins/cds-base/index.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+require __DIR__ .'/vendor/autoload.php';
+
 /**
  * Plugin Name: CDS-SNC Base
  * Plugin URI: https://github.com/cds-snc/platform-mvp
@@ -13,6 +15,39 @@ declare(strict_types=1);
  */
 
 defined('ABSPATH') || exit;
+
+if (!defined('BASE_PLUGIN_NAME')) {
+    define('BASE_PLUGIN_NAME', 'cds-base');
+}
+
+require_once __DIR__ .'/admin-cleaner/index.php';
+
+function cds_plugin_images_url($filename)
+{
+    return plugin_dir_url(__FILE__).'images/'.$filename;
+}
+
+function cds_base_style_admin(): void
+{
+    if (is_super_admin()) {
+        return;
+    }
+    // add stylesheet to the wp admin
+    wp_enqueue_style('cds-base-style', plugin_dir_url(__FILE__).'css/admin.css', [], 1);
+}
+
+add_action('admin_head', 'cds_base_style_admin');
+
+function cds_base_js_admin(): void
+{
+    if (is_super_admin()) {
+        return;
+    }
+    // add stylesheet to the wp admin
+    wp_enqueue_script('blog-customizer', plugins_url('js/admin.js', __FILE__), ['jquery'], '1.0.0', true);
+}
+
+add_action('admin_enqueue_scripts', 'cds_base_js_admin');
 
 /**
  * Load all translations for our plugin from the MO file.
