1f572f68b07304081c847ef4dc93e0eea7b9a0df
Tim Arney <timarney@users.noreply.github.com>
2021-08-27 14:05:03 -0400

    fix post rename (#103)

--- wordpress/wp-content/plugins/cds-base/admin-cleaner/post-rename.php
+++ wordpress/wp-content/plugins/cds-base/admin-cleaner/post-rename.php
@@ -6,29 +6,29 @@ function cds_change_post_label(): void
 {
     global $menu;
     global $submenu;
-    $menu[5][0] = _('Articles', 'cds');
-    $submenu['edit.php'][5][0] = _('Articles', 'cds');
-    $submenu['edit.php'][10][0] = _('Add Article', 'cds');
-    $submenu['edit.php'][16][0] = _('Article Tags', 'cds');
+    $menu[5][0] = __('Articles', 'cds');
+    $submenu['edit.php'][5][0] = __('Articles', 'cds');
+    $submenu['edit.php'][10][0] = __('Add Article', 'cds');
+    $submenu['edit.php'][16][0] = __('Article Tags', 'cds');
 }
 
 function cds_change_post_object(): void
 {
     global $wp_post_types;
     $labels = &$wp_post_types['post']->labels;
-    $labels->name = _('Articles', 'cds');
-    $labels->singular_name = _('Articles', 'cds');
-    $labels->add_new = _('Add Articles', 'cds');
-    $labels->add_new_item = _('Add Articles', 'cds');
-    $labels->edit_item = _('Edit Articles', 'cds');
-    $labels->new_item = _('Articles', 'cds');
-    $labels->view_item = _('View Articles', 'cds');
-    $labels->search_items = _('Search Articles', 'cds');
-    $labels->not_found = _('No Articles found', 'cds');
-    $labels->not_found_in_trash = _('No Articles found in Trash', 'cds');
-    $labels->all_items = _('All Articles', 'cds');
-    $labels->menu_name = _('Articles', 'cds');
-    $labels->name_admin_bar = _('Articles', 'cds');
+    $labels->name = __('Articles', 'cds');
+    $labels->singular_name = __('Articles', 'cds');
+    $labels->add_new = __('Add Articles', 'cds');
+    $labels->add_new_item = __('Add Articles', 'cds');
+    $labels->edit_item = __('Edit Articles', 'cds');
+    $labels->new_item = __('Articles', 'cds');
+    $labels->view_item = __('View Articles', 'cds');
+    $labels->search_items = __('Search Articles', 'cds');
+    $labels->not_found = __('No Articles found', 'cds');
+    $labels->not_found_in_trash = __('No Articles found in Trash', 'cds');
+    $labels->all_items = __('All Articles', 'cds');
+    $labels->menu_name = __('Articles', 'cds');
+    $labels->name_admin_bar = __('Articles', 'cds');
 }
 
 add_action('admin_menu', 'cds_change_post_label');
