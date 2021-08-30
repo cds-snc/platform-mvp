e09b6720bf1a82667ab8a0e265cc1c999eb8d7f4
Daine Trinidad <daine.trinidad@gmail.com>
2021-08-30 08:41:44 -0700

    Import scripts for wp (#89)
    * WIP import scripts for wp
    
    * WIP import scripts for wp
    
    * First attempt at importing posts
    
    * Cleanup
    
    * Pass in a filename
    
    * WIP import scripts for wp
    
    * WIP import scripts for wp
    
    * Importer utility class for processing notify json templates
    
    * Importer utility class for processing notify json templates
    
    * Update package-lock
    
    * Cleanup unused variable
    
    * Change the way we process titles
    
    * Gates `mbind: Operation not permitted` errors
    
    * Major changes to running the script, and parse body for a better title
    
    * Fixed spawn command issues with a queue, and check if post has already been imported
    
    * Renamed files and use es6 modules
    
    * Renamed files and use es6 modules
    
    * Update README.md
    
    * Add linting, and lint import scripts
    
    Co-authored-by: Daine-Rose Trinidad <daine-rosetrinidad@MacBook-Pro.local>

---
+++ import/markdown-converter.js
@@ -0,0 +1,54 @@
+// This script is taken from the gutenberg markdown converter (link below):
+// https://github.com/WordPress/gutenberg/blob/3da717b8d0ac7d7821fc6d0475695ccf3ae2829f/packages/blocks/src/api/raw-handling/markdown-converter.js
+//
+// License:
+//
+// Gutenberg
+// Copyright 2016-2021 by the contributors
+// License for Contributions (on and after April 15, 2021)
+// All code contributed to the Gutenberg project is dual-licensed, and released under both of the following licenses:
+// the GNU General Public License as published by the Free Software Foundation; either version 2 of the License or (at your option) any later version (the “GPL”) and the Mozilla Public License, Version 2.0 (the “MPL”).
+
+/**
+ * External dependencies
+ */
+import showdown from 'showdown'
+
+// Reuse the same showdown converter.
+const converter = new showdown.Converter({
+  noHeaderId: true,
+  tables: true,
+  literalMidWordUnderscores: true,
+  omitExtraWLInCodeBlocks: true,
+  simpleLineBreaks: true,
+  strikethrough: true
+})
+
+/**
+  * Corrects the Slack Markdown variant of the code block.
+  * If uncorrected, it will be converted to inline code.
+  *
+  * @see https://get.slack.help/hc/en-us/articles/202288908-how-can-i-add-formatting-to-my-messages-#code-blocks
+  *
+  * @param {string} text The potential Markdown text to correct.
+  *
+  * @return {string} The corrected Markdown.
+  */
+function slackMarkdownVariantCorrector (text) {
+  return text.replace(
+    /((?:^|\n)```)([^\n`]+)(```(?:$|\n))/,
+    (match, p1, p2, p3) => `${p1}\n${p2}\n${p3}`
+  )
+}
+
+/**
+  * Converts a piece of text into HTML based on any Markdown present.
+  * Also decodes any encoded HTML.
+  *
+  * @param {string} text The plain text to convert.
+  *
+  * @return {string} HTML.
+  */
+export default function markdownConverter (text) {
+  return converter.makeHtml(slackMarkdownVariantCorrector(text))
+}
